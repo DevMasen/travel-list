484a317161402ad6f5a520bb408868369918c706
DevMasen <mohammadwh400@gmail.com>
2025-07-31 18:45:11 +0330

    E71: list and items component rendered static

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './index.css';
 
+const initialItems = [
+	{ id: 1, description: 'Passports', quantity: 2, packed: false },
+	{ id: 2, description: 'Socks', quantity: 12, packed: false },
+];
+
 function App() {
 	return (
 		<div className="app">
@@ -25,14 +30,29 @@ function Form() {
 
 function PackingList() {
 	return (
-		<main className="list">
-			List
-			<Item />
-		</main>
+		<div className="list">
+			<ul>
+				{initialItems.map(item => (
+					<Item item={item} key={item.id} />
+				))}
+			</ul>
+		</div>
 	);
 }
-function Item() {
-	return <div></div>;
+function Item({ item }) {
+	return (
+		<li>
+			<input
+				type="checkbox"
+				id={`${item.id}`}
+				name={item.description}
+			></input>
+			<span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+				{item.quantity} {item.description}
+			</span>
+			<button style={{ color: 'red', fontSize: '40px' }}>&times;</button>
+		</li>
+	);
 }
 function Stats() {
 	return (
