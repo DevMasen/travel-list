6713a35130944e052f1e837748ed67d38ed4975d
DevMasen <mohammadwh400@gmail.com>
2025-08-04 11:21:29 +0330

    E86: sort feature implanted by deriving state

--- src/App.js
+++ src/App.js
@@ -85,10 +85,24 @@ function Form({ onAddItem }) {
 }
 
 function PackingList({ items, onRemoveItem, onPackItem }) {
+	const [sortBy, setSortBy] = useState('input');
+
+	let sortedItems;
+
+	if (sortBy === 'input') sortedItems = items;
+	else if (sortBy === 'alphabet')
+		sortedItems = items
+			.slice()
+			.sort((a, b) => a.description.localeCompare(b.description));
+	else if (sortBy === 'packed')
+		sortedItems = items
+			.slice()
+			.sort((a, b) => Number(b.packed) - Number(a.packed));
+
 	return (
 		<div className="list">
 			<ul>
-				{items.map(item => (
+				{sortedItems.map(item => (
 					<Item
 						item={item}
 						onRemoveItem={onRemoveItem}
@@ -97,6 +111,17 @@ function PackingList({ items, onRemoveItem, onPackItem }) {
 					/>
 				))}
 			</ul>
+
+			<div className="actions">
+				<select
+					value={sortBy}
+					onChange={e => setSortBy(e.target.value)}
+				>
+					<option value="input">Sort by user input</option>
+					<option value="alphabet">Sort by alphabet</option>
+					<option value="packed">Sort by packed</option>
+				</select>
+			</div>
 		</div>
 	);
 }
