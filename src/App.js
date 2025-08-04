f5ad0c44665bb3e95d88504eda2f2511837acef3
DevMasen <mohammadwh400@gmail.com>
2025-08-04 10:51:01 +0330

    E85: Using Derived State to implant the stats section

--- src/App.js
+++ src/App.js
@@ -123,18 +123,26 @@ function Item({ item, onRemoveItem, onPackItem }) {
 	);
 }
 function Stats({ items }) {
+	if (!items.length)
+		return (
+			<p className="stats">
+				Start adding some items to your packing list! 🚀
+			</p>
+		);
+
 	const totalCount = items.length;
 	const packedCount = items.reduce(
 		(acc, item) => (item.packed ? acc + 1 : acc),
 		0
 	);
-	const packedPercent = Math.floor((packedCount / totalCount) * 100);
+	const packedPercent = Math.round((packedCount / totalCount) * 100);
 	return (
 		<footer className="stats">
 			<em>
-				You Have {totalCount} items on your list and you have already
-				packed {packedCount} ({isNaN(packedPercent) ? 0 : packedPercent}
-				%)
+				{packedPercent === 100
+					? 'You got everything! Ready to go ✈️'
+					: `💼 You Have ${totalCount} items on your list and you have already
+				packed ${packedCount} (${packedPercent}%)`}
 			</em>
 		</footer>
 	);
